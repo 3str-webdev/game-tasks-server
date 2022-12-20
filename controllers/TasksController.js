b46c7b6858a3390763738cdc6308ff8e7f67ee13
3str-webdev <3str.webdev@gmail.com>
2022-12-20 23:11:24 +0300

    update: add logs

--- controllers/TasksController.js
+++ controllers/TasksController.js
@@ -4,6 +4,7 @@ const axios = require("axios").default;
 
 class TaskController {
   checkPassword(req, res, next) {
+    console.log(req.body);
     if (req.body.password !== process.env.POST_PASSWORD) {
       res.status(400).json("Неверный пароль");
       return;
@@ -37,6 +38,7 @@ class TaskController {
   }
 
   async getAll(req, res) {
+    console.log(req.body);
     res.json(await TaskController.getAllTasks());
   }
 
